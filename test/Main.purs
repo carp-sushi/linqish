8889970ab06e65c56398385b64318af7bc7ee839
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2022-10-17 16:11:38 -0600

    use newtype for RowB

--- test/Main.purs
+++ test/Main.purs
@@ -45,14 +45,33 @@ tableA =
 -- Table B types - use a record type here 
 --
 
-type RowB = { id :: Int, fk :: Int, active :: Boolean }
+newtype RowB = RowB { id :: Int, fk :: Int, active :: Boolean }
+
+instance eqRowB :: Eq RowB where
+  eq (RowB rb1) (RowB rb2) = rb1.id == rb2.id
+
+instance showRowB :: Show RowB where
+  show (RowB rb) =
+    "RowB { id: "
+      <> (show rb.id)
+      <> ", fk: "
+      <> (show rb.fk)
+      <> ", active: "
+      <> (show rb.active)
+      <> " }"
+
+rowB_fk :: RowB -> Int
+rowB_fk (RowB rb) = rb.fk
+
+rowB_active :: RowB -> Boolean
+rowB_active (RowB rb) = rb.active
 
 tableB :: Array RowB
 tableB =
-  [ { id: 4, fk: 1, active: true }
-  , { id: 3, fk: 2, active: false }
-  , { id: 2, fk: 3, active: true }
-  , { id: 1, fk: 4, active: false }
+  [ RowB { id: 4, fk: 1, active: true }
+  , RowB { id: 3, fk: 2, active: false }
+  , RowB { id: 2, fk: 3, active: true }
+  , RowB { id: 1, fk: 4, active: false }
   ]
 
 --
@@ -67,19 +86,19 @@ selectAll table =
 -- Testing where_
 selectActives :: Array RowB -> Array RowB
 selectActives table =
-  runQuery $ Query (select_ identity) table (where_ _.active)
+  runQuery $ Query (select_ identity) table (where_ rowB_active)
 
 -- Testing select_ with join_
 joinTables :: Array RowA -> Array RowB -> Array (Tuple RowA RowB)
 joinTables a b =
-  runQuery $ Query_ (select_ identity) (join_ a rowA_id b _.fk)
+  runQuery $ Query_ (select_ identity) (join_ a rowA_id b rowB_fk)
 
 activeNames :: Array RowA -> Array RowB -> Query Array (Tuple RowA RowB) String
 activeNames a b =
   Query
     (select_ $ fst >>> rowA_name)
-    (join_ a rowA_id b _.fk)
-    (where_ $ snd >>> _.active)
+    (join_ a rowA_id b rowB_fk)
+    (where_ $ snd >>> rowB_active)
 
 --
 -- Test runner
@@ -93,8 +112,10 @@ main = do
       test "select_ returns all rows" do
         Assert.equal (selectAll tableA) tableA
       test "select_ with where_ filters rows" do
-        let expected = [ { id: 4, fk: 1, active: true }, { id: 2, fk: 3, active: true } ]
-        Assert.equal (selectActives tableB) expected
+        Assert.equal (selectActives tableB)
+          [ RowB { id: 4, fk: 1, active: true }
+          , RowB { id: 2, fk: 3, active: true }
+          ]
       test "select_ with join_ joins all rows" do
         Assert.equal (joinTables tableA tableB) (zip tableA tableB)
       test "select_ with join_ and where_ joins and filters" do
